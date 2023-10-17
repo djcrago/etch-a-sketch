7b4f7580272709e60a95e417c477ccef0e362c1f
Dexter Crago <jodieanddexter@gmail.com>
2023-10-17 15:55:11 -0700

    Remove test text and colors from squares
    Now it is a blank white grid with squares that turn black upon hovering
    over them.

--- javascript.js
+++ javascript.js
@@ -6,21 +6,13 @@ console.log(j);
 for (let i = 0; i < j; i++) {
     const square = document.createElement('div');
     square.classList.toggle('square');
-    square.style.cssText = 'color: blue; background-color: lightblue;';
-    let h = (500/n) + 'px';
-    square.style.height = h;
-    let w = (500/n) + 'px'
-    square.style.width = w;
-    square.textContent = 'sqr';
+    square.style.height = ((500/n) + 'px');
+    square.style.width = ((500/n) + 'px');
     grid.appendChild(square);
 }
-console.log(grid);
 
-const squares = Array.from(grid.querySelectorAll('.square'));
-console.log(squares);
-const sqrs = squares.map((square) => {
+const squares = (Array.from(grid.querySelectorAll('.square'))).map((square) => {
     square.addEventListener('mouseover', () => {
-        square.style.color = 'white';
         square.style.backgroundColor = 'black';
     });
 });
