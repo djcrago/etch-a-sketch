a25d0726fa34f77b91ebea4d2538d092d05c3a56
Dexter Crago <jodieanddexter@gmail.com>
2023-10-17 16:06:42 -0700

    Enable button to clear current squares in grid

--- javascript.js
+++ javascript.js
@@ -3,16 +3,35 @@ let n = 16;
 let j = (n ** 2);
 console.log(j);
 
-for (let i = 0; i < j; i++) {
-    const square = document.createElement('div');
-    square.classList.toggle('square');
-    square.style.height = ((500/n) + 'px');
-    square.style.width = ((500/n) + 'px');
-    grid.appendChild(square);
+function createSquares() {
+    for (let i = 0; i < j; i++) {
+        const square = document.createElement('div');
+        square.classList.toggle('square');
+        square.style.height = ((500/n) + 'px');
+        square.style.width = ((500/n) + 'px');
+        grid.appendChild(square);
+    }
 }
 
+function removeSquares() {
+    for (let i = 0; i < j; i++) {
+        const squares = Array.from(grid.querySelectorAll('.square'));
+        for (square of squares) {
+            grid.removeChild(square);
+        }
+    }
+}
+
+createSquares();
 const squares = (Array.from(grid.querySelectorAll('.square'))).map((square) => {
     square.addEventListener('mouseover', () => {
         square.style.backgroundColor = 'black';
     });
+});
+
+const reset = document.querySelector('#reset');
+console.log(reset);
+reset.addEventListener('click', () => {
+    // n = prompt('Size: ');
+    removeSquares();
 });
