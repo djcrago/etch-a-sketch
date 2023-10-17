788fd789deefa862cb1119ecbb71b87a2f45bdde
Dexter Crago <jodieanddexter@gmail.com>
2023-10-17 15:26:28 -0700

    Correct the number of squares made
    Before Math.sqrt() was used so that the height and width of all the
    squares would add up to the total height and width of the container.
    However we actually want to produce a n x n size grid of squares.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,18 @@
 const grid = document.querySelector('#container');
 let n = 16;
-let j = Math.sqrt(n);
+let j = (n ** 2);
 console.log(j);
 
-for (let i = 0; i < n; i++) {
+for (let i = 0; i < j; i++) {
     const square = document.createElement('div');
     square.style.cssText = 'color: blue; background-color: lightblue;';
-    let h = (500/j) + 'px';
+    let h = (500/n) + 'px';
     square.style.height = h;
-    let w = (500/j) + 'px'
+    let w = (500/n) + 'px'
     square.style.width = w;
-    square.textContent = 'square';
+    square.textContent = 'sqr';
     grid.appendChild(square);
 }
 console.log(grid);
+
+const square = grid.querySelector('div')
