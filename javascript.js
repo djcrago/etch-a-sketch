b428f32e5a79c3082fe126cc65c8f5ac5ab9a74e
Dexter Crago <jodieanddexter@gmail.com>
2023-10-17 18:04:44 -0700

    Add style to web page

--- javascript.js
+++ javascript.js
@@ -1,24 +1,23 @@
 const grid = document.querySelector('#container');
-let gridSize = 16; //size of grid == n x n
-let totalSquares = (gridSize ** 2); //total number of squares in grid
-
-createSquares();
+let screenWidth = (screen.availWidth) / 3; //3 was chosen arbitrarily
+grid.style.cssText = `height: ${(screenWidth + 'px')}; width: ${(screenWidth + 'px')};`;
 const resetButton = document.querySelector('#resetButton');
 resetButton.addEventListener('click', removeSquares);
+let gridSize = 16;
+let totalSquares = (gridSize ** 2);
+createSquares();
 
 function createSquares() {
     for (let i = 0; i < totalSquares; i++) {
         let square = document.createElement('div');
         square.classList.toggle('square');
-        square.style.height = ((500/gridSize) + 'px');
-        square.style.width = ((500/gridSize) + 'px');
+        square.style.cssText = `height: ${((screenWidth/gridSize) + 'px')}; width: ${((screenWidth/gridSize) + 'px')};`
         square.addEventListener('mouseover', () => {
             square.style.backgroundColor = 'black';
         })
         grid.appendChild(square);
     }
 }
-
 function removeSquares() {
     const squares = Array.from(grid.querySelectorAll('.square'));
     for (square of squares) {
