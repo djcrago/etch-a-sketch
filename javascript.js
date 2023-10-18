adb77475d0184f14722f5db23314ca8049d88472
Dexter Crago <jodieanddexter@gmail.com>
2023-10-17 17:04:07 -0700

    Create new grid using size input by user
    Now when the user types in a value into the prompt, a new n x n grid is
    made where n is the value given by the user.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,26 @@
 const grid = document.querySelector('#container');
+const resetButton = document.querySelector('#resetButton');
+
 let n = 16; //size of grid - n x n
 let size = (n ** 2); //total number of squares in grid
-console.log(size);
 
-function createSquares(size) {
-    for (let i = 0; i < size; i++) { //create j number of squares
-        const square = document.createElement('div');
+createSquares();
+
+const squares = (Array.from(grid.querySelectorAll('.square'))).map(colorSquares);
+
+resetButton.addEventListener('click', () => {
+    removeSquares();
+    do {
+        n = +prompt('Size: ');
+    } while (n > 100 || n < 1);
+    size = (n ** 2);
+    createSquares();
+    const squares = (Array.from(grid.querySelectorAll('.square'))).map(colorSquares);
+});
+
+function createSquares() {
+    for (let i = 0; i < size; i++) { //create size number of squares
+        let square = document.createElement('div');
         square.classList.toggle('square');
         square.style.height = ((500/n) + 'px');
         square.style.width = ((500/n) + 'px');
@@ -13,26 +28,16 @@ function createSquares(size) {
     }
 }
 
-function removeSquares() {
-    const squares = Array.from(grid.querySelectorAll('.square'));
-    for (square of squares) { //remove all squares from grid
-        grid.removeChild(square);
-    }
-}
-
-createSquares(size);
-const squares = (Array.from(grid.querySelectorAll('.square'))).map((square) => {
+function colorSquares(square) {
     square.addEventListener('mouseover', () => {
         square.style.backgroundColor = 'black';
     });
-});
+}
 
-const reset = document.querySelector('#reset');
-console.log(reset);
-reset.addEventListener('click', () => {
-    removeSquares();
-    let newSize
-    do {
-        newSize = +prompt('Size: ');
-    } while (newSize > 100 || newSize < 1);
-});
+function removeSquares() {
+    const squares = Array.from(grid.querySelectorAll('.square'));
+    for (square of squares) { //remove all squares from grid
+        square.classList.toggle('square');
+        grid.removeChild(square);
+    }
+}
