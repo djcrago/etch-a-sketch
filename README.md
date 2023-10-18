9213855e92e2174e14ec1404e2d8779cdb548206
Dexter Crago <jodieanddexter@gmail.com>
2023-10-18 10:57:48 -0700

    Clean up code and update README file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# etch-a-sketch
+Below is a summary of all the things I learned or practiced in creating this website.
+
+- Referencing elements with querySelector
+- Setting cssText in JS
+- Creating elements in JS
+- Setting attributes in JS
+- Adding EventListeners
+- Appending and removing children in JS
+- Assigning rbg values randomly
+- Incrementing opacity values over multiple interactions
+- Creating a grid of squares using input by the user
+- Creating an array from a node list
--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 const grid = document.querySelector('#container');
     let gridWidth = (screen.availWidth) / 3; //3 was chosen arbitrarily
-    grid.style.cssText = `height: ${(gridWidth + 'px')}; width: ${(gridWidth + 'px')};`; //dynamically determine grid size
+    grid.style.cssText = `height: ${(gridWidth + 'px')}; width: ${(gridWidth + 'px')};`; //dynamically determine grid size (not for mobile)
 const startDrawing = document.createElement('button');
     startDrawing.classList.toggle('btn'); //adds style from css
     startDrawing.textContent = 'Start Drawing!';
-    grid.appendChild(startDrawing);
     startDrawing.addEventListener('click', () => {
         createSquares();
         grid.removeChild(startDrawing);
     });
+    grid.appendChild(startDrawing);
 const resetButton = document.querySelector('#resetButton');
     resetButton.addEventListener('click', removeSquares);
 let gridSize = 16; //16 was chosen arbitrarily
@@ -19,13 +19,13 @@ let totalSquares = (gridSize ** 2);
 function createSquares() {
     for (let i = 0; i < totalSquares; i++) {
         let square = document.createElement('div');
-        square.classList.toggle('square');
-        square.style.cssText = `height: ${((gridWidth/gridSize) + 'px')}; width: ${((gridWidth/gridSize) + 'px')};`
-        let a = 0;
+            square.classList.toggle('square'); //so we can find it in removeSquares
+            square.style.cssText = `height: ${((gridWidth/gridSize) + 'px')}; width: ${((gridWidth/gridSize) + 'px')};`
         let randomColor = addColor();
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = `rgba(${randomColor[0]}, ${randomColor[1]}, ${randomColor[2]}, ${a += 0.1})`;
-        });
+            let a = 0;
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = `rgba(${randomColor[0]}, ${randomColor[1]}, ${randomColor[2]}, ${a += 0.1})`;
+            });
         grid.appendChild(square);
     }
 }
@@ -33,16 +33,15 @@ function createSquares() {
 function removeSquares() {
     do {
         gridSize = +prompt('New board = size x size\nSize: ');
-        if (gridSize === 0) {
+        if (gridSize === 0) { //allows user to cancel board reset
             return;
         }
     } while (gridSize > 100 || gridSize < 1);
-    totalSquares = (gridSize ** 2);
+        totalSquares = (gridSize ** 2);
     const squares = Array.from(grid.querySelectorAll('.square'));
-    for (square of squares) {
-        square.classList.toggle('square');
-        grid.removeChild(square);
-    }
+        for (square of squares) {
+            grid.removeChild(square);
+        };
     createSquares();
 }
 
