6d1ea5f29c0e594934284b9f0051118fec24baf1
Dexter Crago <jodieanddexter@gmail.com>
2024-04-29 20:14:02 -0600

    Refactor etch-a-sketch project
    I refactored this project later on with better design and organization.

--- javascript.js
+++ javascript.js
@@ -1,54 +1,87 @@
-const grid = document.querySelector('#container');
-    let gridWidth = (screen.availWidth) / 3; //3 was chosen arbitrarily
-    grid.style.cssText = `height: ${(gridWidth + 'px')}; width: ${(gridWidth + 'px')};`; //dynamically determine grid size (not for mobile)
-const startDrawing = document.createElement('button');
-    startDrawing.classList.toggle('btn'); //adds style from css
-    startDrawing.textContent = 'Start Drawing!';
-    startDrawing.addEventListener('click', () => {
-        createSquares();
-        grid.removeChild(startDrawing);
-    });
-    grid.appendChild(startDrawing);
+const grid = document.querySelector('#grid');
+
+const defaultCellsInARow = 16;
+
+const startDrawingBtn = document.createElement('button');
+
+startDrawingBtn.textContent = 'Start Drawing!';
+startDrawingBtn.addEventListener('click', () => {
+  grid.removeChild(startDrawingBtn);
+  populateGrid(defaultCellsInARow);
+});
+
+grid.appendChild(startDrawingBtn);
+
 const resetButton = document.querySelector('#resetButton');
-    resetButton.addEventListener('click', removeSquares);
-let gridSize = 16; //16 was chosen arbitrarily
-let totalSquares = (gridSize ** 2);
+resetButton.addEventListener('click', handleReset);
 
+// Functions
 
+function populateGrid(cellsInARow) {
+  const gridWidth = 450;
+  const cellWidth = gridWidth / cellsInARow;
 
-function createSquares() {
-    for (let i = 0; i < totalSquares; i++) {
-        let square = document.createElement('div');
-            square.classList.toggle('square'); //so we can find it in removeSquares
-            square.style.cssText = `height: ${((gridWidth/gridSize) + 'px')}; width: ${((gridWidth/gridSize) + 'px')};`
-        let randomColor = addColor();
-            let a = 0;
-            square.addEventListener('mouseover', () => {
-                square.style.backgroundColor = `rgba(${randomColor[0]}, ${randomColor[1]}, ${randomColor[2]}, ${a += 0.1})`;
-            });
-        grid.appendChild(square);
-    }
+  const totalNumberOfCells = cellsInARow ** 2;
+
+  for (let i = 0; i < totalNumberOfCells; i += 1) {
+    const randomColor = addColor();
+    const cell = createCell(cellWidth, randomColor);
+    grid.appendChild(cell);
+  }
 }
 
-function removeSquares() {
-    do {
-        gridSize = +prompt('New board = size x size\nSize: ');
-        if (gridSize === 0) { //allows user to cancel board reset
-            return;
-        }
-    } while (gridSize > 100 || gridSize < 1);
-        totalSquares = (gridSize ** 2);
-    const squares = Array.from(grid.querySelectorAll('.square'));
-        for (square of squares) {
-            grid.removeChild(square);
-        };
-    createSquares();
+function addColor() {
+  const rgb = [];
+
+  for (let i = 0; i < 3; i += 1) {
+    rgb.push(Math.floor(Math.random() * 255));
+  }
+  rgb.push(0);
+
+  return rgb;
 }
 
-function addColor () {
-    let rgb = [0, 0, 0];
-    for (let i = 0; i < rgb.length; i++) {
-        rgb[i] = Math.floor(Math.random() * 255);
-    }
-    return rgb;
+function createCell(cellWidth, cellColor) {
+  const cell = document.createElement('div');
+
+  cell.style.cssText = `height: ${cellWidth}px; width: ${cellWidth}px;`;
+  cell.addEventListener('mouseover', () => {
+    const rgb = `${cellColor[0]}, ${cellColor[1]}, ${cellColor[2]}`;
+    let a = cellColor[3];
+    a += 0.1;
+    cellColor.splice(3, 1, a);
+
+    cell.style.backgroundColor = `rgba(${rgb}, ${a})`;
+  });
+
+  return cell;
+}
+
+function handleReset() {
+  const newCellsInARow = getNewGridSize();
+
+  if (newCellsInARow) {
+    removeCells();
+    populateGrid(newCellsInARow);
+  }
+}
+
+function getNewGridSize() {
+  let valid = true;
+
+  const newCellsInARow = +prompt('New board size: (size x size)\nSize: ');
+
+  if (newCellsInARow > 100 || newCellsInARow < 1) {
+    valid = false;
+    alert('Size must be between 1 and 100');
+  }
+
+  if (valid) return newCellsInARow;
+  if (!valid) return null;
+}
+
+function removeCells() {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
 }
